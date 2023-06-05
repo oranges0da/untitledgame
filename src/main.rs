9021358d1de74033e3bd0dded94a7756d155a68a
Alex Ionitel <oranggsoda@gmail.com>
2023-06-05 16:05:40 -0400

    spawning window working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 
 fn main() {
-    App::new().add_startup_system(setup).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Popcorn Guy".into(),
+                resolution: (640., 480.).into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(setup)
+        .run();
 }
 
-fn setup(commands: Commands) {
-    println!("Hello World!");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
