aa14440a6e147afd040120f01b046da356beeb9e
Alex Ionitel <oranggsoda@gmail.com>
2023-06-05 15:46:46 -0400

    bevy working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("This is the popcorn_guy game!");
+    App::new().add_startup_system(setup).run();
+}
+
+fn setup(commands: Commands) {
+    println!("Hello World!");
 }
