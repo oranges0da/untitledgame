adbc90ed70c850bb8d0f3bb20f67f43e6c8b8cbf
Alex Ionitel <oranggsoda@gmail.com>
2023-06-06 17:26:12 -0400

    player movement implementation working

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
-pub const PLAYER_SIZE: f32 = 4.; // factor to enlarge the player sprite
+const PLAYER_SIZE: f32 = 4.; // factor to enlarge the player sprite
+const PLAYER_SPEED: f32 = 100.;
 
 #[derive(Component)]
 pub struct Player {}
@@ -10,7 +11,7 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
+        app.add_startup_system(spawn_player).add_system(move_player);
     }
 }
 
@@ -43,4 +44,45 @@ fn spawn_player(
         },
         Player {},
     ));
+}
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_transform: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    // getting mutable player_transform property for every SINGLE frame (get_single_mut makes sense now.)
+    if let Ok(mut player_pos) = player_transform.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up) {
+            info!("Up is pressed");
+            direction += Vec3::new(0., 1., 0.);
+        }
+
+        if keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down) {
+            info!("Up is pressed");
+            direction += Vec3::new(0., -1., 0.);
+        }
+
+        if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
+            info!("Up is pressed");
+            direction += Vec3::new(-1., 0., 0.);
+        }
+
+        if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
+            info!("Up is pressed");
+            direction += Vec3::new(1., 0., 0.);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize(); // allows sprite to move diagonally
+        }
+
+        // setting translation property to our own updated direction vector
+        // delta_seconds returns time elapsed since last frame, used to make movement frame-rate independent
+        player_pos.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    } else {
+        info!("Could not parse player_transform");
+    }
 }
