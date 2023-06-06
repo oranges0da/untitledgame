e2dd02375fd4d98354c513687ea3ded7af737a1a
Alex Ionitel <oranggsoda@gmail.com>
2023-06-06 16:15:13 -0400

    better comments and cropped spritesheet

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Popcorn Guy".into(),
+                        resolution: (640., 480.).into(),
+                        present_mode: PresentMode::AutoVsync,
+                        // Tells wasm to resize the window according to the available canvas
+                        fit_canvas_to_parent: true,
+                        // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                        prevent_default_event_handling: false,
                         ..default()
                     }),
                     ..default()
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-pub const PLAYER_SIZE: f32 = 10.; // factor to enlarge player sprite
+pub const PLAYER_SIZE: f32 = 4.; // factor to enlarge the player sprite
 
 #[derive(Component)]
 pub struct PlayerPlugin;
@@ -16,19 +16,25 @@ fn spawn_player(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
+    // load spritesheet and split into grid of individual sprites
     let texture_handle = asset_server.load("spritesheet.png");
-    let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(40., 15.), 4, 4, None, None);
-
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(30., 16.),
+        4,
+        5,
+        Some(Vec2::new(0., 0.)),
+        Some(Vec2::new(0., 0.)),
+    );
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands.spawn(SpriteSheetBundle {
         texture_atlas: texture_atlas_handle,
         sprite: TextureAtlasSprite {
-            index: 8, // which sprite to spawn
+            index: 4, // index of which sprite to spawn in sheet
             ..default()
         },
-        transform: Transform::from_scale(Vec3::splat(PLAYER_SIZE)),
+        transform: Transform::from_scale(Vec3::new(PLAYER_SIZE, PLAYER_SIZE, 0.)), // make sprite bigger by a factor of PLAYER_SIZE
         ..default()
     });
 }
