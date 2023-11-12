58a38bfb6b8bf7301f7eee12eb2588a10375d75d
Alex Ionitel <oranggsoda@gmail.com>
2023-11-11 19:59:40 -0500

    playing same animation when both jumping and falling for now

--- src/animation.rs
+++ src/animation.rs
@@ -125,9 +125,10 @@ fn change_player_animation(
             && !keyboard_input.any_pressed([KeyCode::W, KeyCode::Up]) && player_transform.translation.y <= 0.
     {
         Animation::Run
-    } else if keyboard_input.any_just_pressed([KeyCode::W, KeyCode::Up, KeyCode::Space]) {
-        Animation::Jump
-    } else if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up]) {
+    } else if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up])
+        // if player is above ground (jumping or falling)
+        || player_transform.translation.y > 0.
+    {
         Animation::Jump
     } else if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up])
         && keyboard_input.any_pressed([KeyCode::A, KeyCode::D, KeyCode::Left, KeyCode::Right])
