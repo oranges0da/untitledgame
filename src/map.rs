20737f80d81900c0c0b3d41a7ce3092854711536
Alex Ionitel <oranggsoda@gmail.com>
2023-11-13 17:33:52 -0500

    spawning single tile for now, acting as ground for physics

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,4 @@
+use crate::globals;
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
@@ -18,13 +19,35 @@ fn spawn_bg(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
-fn spawn_map(mut commands: Commands) {
-    // spawn ground
+fn spawn_map(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let size: f32 = 24.; // size of single tile
+
+    let texture_handle = asset_server.load("map/tiles.png");
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(size, size),
+        24,
+        22,
+        Some(Vec2::new(1., 1.)),
+        None,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
     commands
         .spawn(SpriteSheetBundle {
-            transform: Transform::from_translation(Vec3::new(0., -50., 0.)),
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite {
+                index: 1,
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0., -100., 1.))
+                .with_scale(Vec3::new(2., 2., 0.)),
             ..default()
         })
         .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(100., 30.));
+        .insert(Collider::cuboid(12., 12.));
 }
