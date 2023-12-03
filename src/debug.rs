a302329cda130a64aa9948717365db2925568055
Alex Ionitel <oranggsoda@gmail.com>
2023-12-02 21:47:05 -0500

    drawing fps to screen accurately

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,6 @@
 use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use std::fmt::Write;
 
 pub struct DebugPlugin;
 
@@ -8,45 +9,63 @@ impl Plugin for DebugPlugin {
         app.init_resource::<FrameRate>()
             .add_plugins(LogDiagnosticsPlugin::default())
             .add_plugins(FrameTimeDiagnosticsPlugin::default())
-            .add_systems(Update, draw_average_fps);
+            .add_systems(Startup, spawn_fps)
+            .add_systems(Update, update_fps)
+            .add_systems(Update, update_fps_text);
     }
 }
 
 #[derive(Resource, Default)]
-pub struct FrameRate(pub f64);
+struct FrameRate(f64);
 
-// Draw current FPS to corner of screen.
-fn draw_average_fps(
-    mut commands: Commands,
-    diagnostics: Res<DiagnosticsStore>,
-    mut frame_rate: ResMut<FrameRate>,
-) {
-    // Get average FPS from Bevy's diagnostics store.
-    if let Some(fps) = diagnostics
-        .get(FrameTimeDiagnosticsPlugin::FPS)
-        .and_then(|fps| fps.average())
-    {
-        frame_rate.0 = fps
-    } else {
-        info!("Could not set frame_rate resource in main.");
-    }
+#[derive(Component)]
+struct FrameRateText;
 
-    commands.spawn(TextBundle {
-        text: Text::from_section(
-            format!("FPS: {}", frame_rate.0),
-            TextStyle {
-                color: Color::WHITE,
+// Spawn FPS in corner of screen.
+fn spawn_fps(mut commands: Commands, frame_rate: Res<FrameRate>) {
+    commands
+        .spawn(TextBundle {
+            text: Text::from_section(
+                format!("FPS: {}", frame_rate.0),
+                TextStyle {
+                    color: Color::WHITE,
+                    ..default()
+                },
+            ),
+            style: Style {
+                display: Display::Flex,
+                position_type: PositionType::Absolute,
+                // Offset from top-left corner.
+                top: Val::Px(10.),
+                left: Val::Px(10.),
                 ..default()
             },
-        ),
-        style: Style {
-            display: Display::Flex,
-            position_type: PositionType::Absolute,
-            // Offset from top-left corner.
-            top: Val::Px(10.),
-            left: Val::Px(10.),
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(FrameRateText);
+}
+
+// Update FPS to current average.
+fn update_fps(mut frame_rate: ResMut<FrameRate>, diagnostics: Res<DiagnosticsStore>) {
+    let Some(fps) = diagnostics
+        .get(FrameTimeDiagnosticsPlugin::FPS)
+        .and_then(|fps| fps.average())
+    else {
+        return;
+    };
+
+    frame_rate.0 = fps;
+}
+
+// Update FPS text on screen.
+fn update_fps_text(
+    mut text_query: Query<&mut Text, With<FrameRateText>>,
+    frame_rate: Res<FrameRate>,
+) {
+    for mut text in text_query.iter_mut() {
+        let value = &mut text.sections[0].value;
+        value.clear();
+
+        write!(value, "FPS: {:.0}", frame_rate.0).unwrap();
+    }
 }
