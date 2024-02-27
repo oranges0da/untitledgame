99c3a049d6982cd7298f6a8cc7b3c79ee3031d60
Alex Ionitel <oranggsoda@gmail.com>
2024-02-27 18:29:55 -0500

    spawning row of grass blocks

--- src/map.rs
+++ src/map.rs
@@ -24,7 +24,10 @@ fn spawn_map(
 ) {
     const TILE_SIZE: f32 = 16.; // Size of single tile in tileset.
     const SCALE: f32 = 3.;
+    const SCALED_TILE_SIZE: f32 = SCALE * TILE_SIZE;
+    const MAP_SIZE: i32 = 20;
 
+    // Cut out certain tile from tileset.
     let texture_handle = asset_server.load("map/tiles.png");
     let texture_atlas = TextureAtlas::from_grid(
         texture_handle,
@@ -36,33 +39,24 @@ fn spawn_map(
     );
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    // Spawn two tiles side by side.
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle.clone(),
-        transform: Transform {
-            scale: Vec3::new(SCALE, SCALE, 0.),
-            ..default()
-        },
-        sprite: TextureAtlasSprite {
-            index: 1,
-            ..default()
-        },
-        ..default()
-    })
-    .insert(GroundTile::Grass);
+    // Generate row of blocks.
+    for x in 0..MAP_SIZE {
+        let x_offset: f32 = (x as f32 * SCALED_TILE_SIZE * 0.5);
+        let y_offset: f32 = (x as f32 * SCALED_TILE_SIZE * 0.25);
 
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle.clone(),
-        transform: Transform {
-            translation: Vec3::new(32., -32., 0.),
-            scale: Vec3::new(SCALE, SCALE, 0.),
+        commands.spawn(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle.clone(),
+            transform: Transform {
+                translation: Vec3::new(SCALED_TILE_SIZE + x_offset, -SCALED_TILE_SIZE - y_offset, 0.),
+                scale: Vec3::new(SCALE, SCALE, 0.),
+                ..default()
+            },
+            sprite: TextureAtlasSprite {
+                index: 1,
+                ..default()
+            },
             ..default()
-        },
-        sprite: TextureAtlasSprite {
-            index: 1,
-            ..default()
-        },
-        ..default()
-    })
-    .insert(GroundTile::Grass);
+        })
+        .insert(GroundTile::Grass);
+    }
 }
